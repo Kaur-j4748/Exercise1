007b16f91ac8ce97a901981eb11db3e35a2d7e18
Jaspreet Kaur <kaurj95@student.douglascollege.ca>
2020-05-22 21:39:17 -0700

    New Changes

--- src/myJavaProject/myProject.java
+++ src/myJavaProject/myProject.java
@@ -1,20 +1,41 @@
 /**
- * 
+ * Software Engineering - Summer 2020
  */
 package myJavaProject;
+import java.util.Scanner;
 
-/**
- * @author JASSU
- *
- */
 public class myProject {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("Enter an Integer Number: ");
+		// input
+		num = input.nextInt();
+		
+		//result
+		System.out.print("Answer :");
+		evenOdd(num);
+		System.out.println();
+		
 
 	}
+	
+	//Method to find out even or odd number
+	public static int evenOdd(int num) {
+		String n = "";
+		
+		if(num % 2 == 0) {
+			n = num + "is an even number";
+		
+	}
+	else {
+		n = num + "is an odd number";
+	}
+	
+	return num;
 
+}
 }
