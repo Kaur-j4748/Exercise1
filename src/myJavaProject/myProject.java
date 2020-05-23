c78503c9774e1dd747f40514a966379eb3d63526
Jaspreet Kaur <kaurj95@student.douglascollege.ca>
2020-05-22 21:18:53 -0700

    New Java Project

---
+++ src/myJavaProject/myProject.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author JASSU
+ *
+ */
+public class myProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
